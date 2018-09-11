98a1b03dbd6274a07ae20395b1bbe0464e2606eb
kevin.anderson <nlaoshi830.gmail.com>
2018-09-11 19:19:15 +0900

    Add Index, Create, New, EncodingFilter

--- src/controllers/NewServlet.java
+++ src/controllers/NewServlet.java
@@ -2,7 +2,6 @@ package controllers;
 
 import java.io.IOException;
 
-import javax.persistence.EntityManager;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -10,22 +9,21 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.catalina.connector.Request;
+
 import models.Task;
-import utils.DBUtil;
-import java.util.List;
-import javax.persistence.EntityManager;
 
 /**
- * Servlet implementation class IndexServlet
+ * Servlet implementation class NewServlet
  */
-@WebServlet("/index")
-public class IndexServlet extends HttpServlet {
+@WebServlet("/new")
+public class NewServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public IndexServlet() {
+    public NewServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -34,16 +32,12 @@ public class IndexServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	    EntityManager em = DBUtil.createEntityManager();
+		request.setAttribute("_token", request.getSession().getId());
+		request.setAttribute("task", new Task());
 
-	    List<Task> tasks = em.createNamedQuery("getAllTasks", Task.class).getResultList();
+		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/tasks/new.jsp");
+		rd.forward(request, response);
 
-	    em.close();
-
-	    request.setAttribute("tasks", tasks);
-
-	    RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/tasks/index.jsp");
-	    rd.forward(request, response);
 	}
 
 }
